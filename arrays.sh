53a081ecb676beb1ade69fb3522013c43c2fcbd8
minahil <260474360+minahilkhan67@users.noreply.github>
2026-05-08 10:04:35 -0500

    Added arrays script

--- arrays.sh
+++ arrays.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-# Arrays store multiple values
+# Arrays store multiple values.
 
-fruits=("apple" "banana" "apple")
+fruits=("apple" "banana" "mango")
 
 echo "First fruit: ${fruits[0]}"
 
-# Loop through array
 for fruit in "${fruits[@]}"
 do
     echo $fruit
 done
 
-# Common issue:
-# Forgetting quotes around ${fruits[@]}
+# Troubleshooting:
+# Use quotes around ${fruits[@]}
