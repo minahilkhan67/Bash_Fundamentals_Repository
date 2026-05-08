5a0b7f4114c74bbdb948e5e72ef6f4c3bbfb0449
minahil <260474360+minahilkhan67@users.noreply.github>
2026-05-08 10:00:49 -0500

    Added brackets script

--- brackets.sh
+++ brackets.sh
@@ -1,16 +1,13 @@
 #!/bin/bash
 
-# Brackets are used for conditions
+# Brackets are used for conditions.
 
-num=10
+number=10
 
-if [ $num -gt 5 ]
+if [ $number -gt 5 ]
 then
     echo "Greater than 5"
 fi
 
-# Common issue:
-# Missing spaces inside []
-
-# [$num -gt 5]
-# [ $num -gt 5 ]
+# Troubleshooting:
+# Leave spaces inside brackets.
