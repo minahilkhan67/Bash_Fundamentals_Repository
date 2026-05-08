66bb37c8ecc0a484226dea8f48bf7e2a128735ef
minahil <260474360+minahilkhan67@users.noreply.github>
2026-05-08 10:02:11 -0500

    Added input script

--- input.sh
+++ input.sh
@@ -1,24 +1,13 @@
-```bash id="ih0c5x"
 #!/bin/bash
 
-# User input lets people type information
-# into the terminal using the read command.
+# The read command gets user input.
 
 echo "Enter your name:"
 read name
 
-# The input is stored in the variable 'name'
-
 echo "Hello, $name"
 
-# Common mistake:
-# Forgetting to add a variable after read
-
-# read
-# read name
-
 # Troubleshooting:
-# Make sure the variable name matches in echo
-
+# Make sure variable names match.
 
 
