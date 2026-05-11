aea952d6a126dbbe23bc838645471cd6bc33cf06
minahil <260474360+minahilkhan67@users.noreply.github>
2026-05-11 10:12:29 -0500

    Added for loops script

---
+++ for_loops.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# For loops repeat code multiple times.
+
+for i in {1..5}
+do
+    echo "Number: $i"
+done
+
+# How it works:
+# The loop repeats once for each number.
+
+# Common issue:
+# Incorrect loop syntax.
+
+# Troubleshooting:
+# Check brackets and spacing carefully.
