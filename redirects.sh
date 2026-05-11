e7937ef4620831922748f9e4615a169255a4f454
minahil <260474360+minahilkhan67@users.noreply.github>
2026-05-11 10:05:33 -0500

    Added redirects script

---
+++ redirects.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# Redirects send output into files.
+
+echo "This is saved" > file.txt
+
+# >> adds text without replacing old text.
+
+echo "Another line" >> file.txt
+
+# Common issue:
+# Accidentally overwriting files.
+
+# Troubleshooting:
+# Use >> when appending text.
