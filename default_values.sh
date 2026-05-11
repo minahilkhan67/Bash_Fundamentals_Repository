3eaecd77d66d7ac90617316e12a22c6071161ed9
minahil <260474360+minahilkhan67@users.noreply.github>
2026-05-11 10:03:42 -0500

    Added default values script

---
+++ default_values.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Default values are used when no input is provided.
+
+name=${1:-"Guest"}
+
+echo "Hello, $name"
+
+# How it works:
+# If no value is entered, Guest becomes the default.
+
+# Common issue:
+# Incorrect variable syntax.
+
+# Troubleshooting:
+# Use ${variable:-default}
