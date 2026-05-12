4d8f351686c14892738de0628be0b19281ae2523
minahil <260474360+minahilkhan67@users.noreply.github>
2026-05-12 10:16:35 -0500

    Added explanation comments for while loops

--- while_loop.sh
+++ while_loop.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-# While loops repeat while a condition is true.
+# While loops repeat code while
+# a condition remains true.
 
 count=1
+
+# The loop continues until
+# the condition becomes false.
 
 while [ $count -le 5 ]
 do
     echo "Count: $count"
+
+    # count++ increases the value by 1.
     ((count++))
 done
 
 # Common issue:
-# Infinite loops.
+# Infinite loops happen when
+# the condition never changes.
 
 # Troubleshooting:
-# Make sure the condition changes.
+# Make sure the counter updates correctly.
