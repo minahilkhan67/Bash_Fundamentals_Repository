473527a41f3591b9456b235f3392157bdddbf171
minahil <260474360+minahilkhan67@users.noreply.github>
2026-05-12 10:29:42 -0500

    Added troubleshooting notes for printf

--- printf.sh
+++ printf.sh
@@ -3,6 +3,7 @@
 # printf formats output neatly.
 # printf gives more control over formatting than echo.
 # %s is used for strings in printf.
+# Formatting errors can happen with incorrect specifiers.
 
 name="Minahil"
 
